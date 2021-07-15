a557b7c005c92801a14e350b71451d0a1bc83766
Luiz Eduardo <luizeduardorodrigues807@gmail.com>
2021-07-15 12:16:59 -0300

    Melhoria no sistema de calculo do site e ajustes no html

--- js/calculadora.js
+++ js/calculadora.js
@@ -1,10 +1,10 @@
 function clicar(){
-    var peso = document.querySelector(".peso").value;
-    var altura = document.querySelector(".altura").value;
+    let peso = document.querySelector(".peso").value;
+    let altura = document.querySelector(".altura").value;
 
     if(peso !== '' && altura !== ''){
         altura = altura / 100;
-        var imc = peso / Math.pow(altura, 2);
+        let imc = peso / Math.pow(altura, 2);
         var arredondado = parseFloat(imc.toFixed(2));
         document.querySelector("strong").innerHTML = arredondado;
     }else{
@@ -14,46 +14,69 @@ function clicar(){
     }
 
     if( arredondado < 17 ){
-            document.querySelector('.linha1').style.backgroundColor = "black";
-            document.querySelector(".linha1").style.transition = "all .5s";
-            document.querySelector(".linha1").style.color = "white";
-            document.querySelector(".como-esta-a-saude").innerHTML = "Magreza Grave";
+        let linha1 = document.querySelector(".linha1");
+        let saude = document.querySelector(".como-esta-a-saude");
+
+        linha1.style.backgroundColor = "black";
+        linha1.style.transition = ".5s";
+        linha1.style.color = "white";
+        saude.innerHTML = "Magreza Grave";
     }else{
         if( arredondado >= 17 && arredondado < 18.5 ){
-            document.querySelector('.linha2').style.backgroundColor = "black";
-            document.querySelector(".linha2").style.transition = "all .5s";
-            document.querySelector(".linha2").style.color = "white";
-            document.querySelector(".como-esta-a-saude").innerHTML = "Magreza Leve";
+            let linha2 = document.querySelector(".linha2");
+            let saude = document.querySelector(".como-esta-a-saude");
+
+            linha2.style.backgroundColor = "black";
+            linha2.style.transition = ".5s";
+            linha2.style.color = "white";
+            saude.innerHTML = "Magreza Leve";
         }else{
-            if( arredondado >= 18.5 && arredondado < 25){
-                document.querySelector(".linha3").style.backgroundColor = "black";
-                document.querySelector(".linha3").style.color = "white";
-                document.querySelector(".linha3").style.transition = "all .5s";
-                document.querySelector(".como-esta-a-saude").innerHTML = "Saudável";
+            if(arredondado >= 18.5 && arredondado < 25){
+                let linha3 = document.querySelector(".linha3");
+                let saude = document.querySelector(".como-esta-a-saude");
+
+                linha3.style.backgroundColor = "black";
+                linha3.style.transition = ".5s";
+                linha3.style.color = "white";
+                saude.innerHTML = "Saudável";
             }else{
                 if( arredondado >= 25 && arredondado < 30){
-                    document.querySelector(".linha4").style.backgroundColor = "black";
-                    document.querySelector(".linha4").style.color = "white";
-                    document.querySelector(".linha4").style.transition = "all .5s";
-                    document.querySelector(".como-esta-a-saude").innerHTML = "Sobrepeso";
+                    let linha4 = document.querySelector(".linha4");
+                    let saude = document.querySelector(".como-esta-a-saude");
+
+                    linha4.style.backgroundColor = "black";
+                    linha4.style.transition = ".5s";
+                    linha4.style.color = "white";
+                    saude.innerHTML = "Sobrepeso";
                 }else{
                     if( arredondado >= 30 && arredondado < 35){
-                        document.querySelector(".linha5").style.backgroundColor = "black";
-                        document.querySelector(".linha5").style.transition = "all .5s";
-                        document.querySelector(".linha5").style.color = "white";
-                        document.querySelector(".como-esta-a-saude").innerHTML = "Obesidade de Grau 1";
+                        let linha5 = document.querySelector(".linha5");
+                        let saude = document.querySelector(".como-esta-a-saude");
+
+                        linha5.style.backgroundColor = "black";
+                        linha5.style.transition = ".5s";
+                        linha5.style.color = "white";
+                        saude.innerHTML = "Obesidade de Grau 1";
                     }else{
                         if( arredondado >= 35 && arredondado < 40){
-                            document.querySelector(".linha6").style.backgroundColor = "black";
-                            document.querySelector(".linha6").style.transition = "all .5s";
-                            document.querySelector(".linha6").style.color = "white";
-                            document.querySelector(".como-esta-a-saude").innerHTML = "Obesidade Grau 2(severa)";
+                            let linha6 = document.querySelector('.linha6');
+                            let saudee = document.querySelector(".como-esta-a-saude");
+
+                            linha6.style.backgroundColor = "black";
+                            linha6.style.transition = ".5s";
+                            linha6.style.color = "white";
+                            saudee.innerHTML = "Obesidade Grau 2(severa)";
+                            saudee.style.fontSize = "1.5rem";
                         }else{
                             if( arredondado > 40){
-                                document.querySelector(".linha7").style.backgroundColor = "black";
-                                document.querySelector(".linha7").style.transition = "all .5s";
-                                document.querySelector(".linha7").style.color = "white";
-                                document.querySelector(".como-esta-a-saude").innerHTML = "Obesidade Grau 3(mórbida)";
+                                let linha7 = document.querySelector(".linha7");
+                                let saude = document.querySelector(".como-esta-a-saude");
+
+                                linha7.style.backgroundColor = "black";
+                                linha7.style.transition = ".5s";
+                                linha7.style.color = "white";
+                                saude.innerHTML = "Obesidade Grau 3(mórbida)";
+                                saude.style.fontSize = "1.4rem";
                             }
                         }
                     }
@@ -65,10 +88,10 @@ function clicar(){
 
 
 function limpar(){
-    document.querySelector("strong").innerHTML = '';
-    document.querySelector(".peso").value = ''; 
-    document.querySelector(".altura").value = '';
-    document.querySelector(".como-esta-a-saude").innerHTML = "";
+    let strongg = document.querySelector("strong").innerHTML = '';
+    let pesso = document.querySelector(".peso").value = '';
+    let alturaa = document.querySelector(".altura").value = '';
+    let saude = document.querySelector(".como-esta-a-saude").innerHTML = "";
 
     /* Resetando os itens da tabela */
     document.querySelector(".linha1").style = '';
@@ -85,4 +108,8 @@ function limpar(){
     document.querySelector(".linha6").style.transition = "all .5s";
     document.querySelector(".linha7").style = '';
     document.querySelector(".linha7").style.transition = "all .5s";
+
+    if(strongg === '' &&  pesso === '' && alturaa === '' && saude === ''){
+        strongg.document.style.fontSize = "1.7rem";
+    }
 }
